d5a8231bea1d1667cd0f4c312f9fc5700c9b64c6
mbera124 <josephmbera124@gmail.com>
2022-03-29 07:42:17 +0300

    deploy app

---
+++ build/precache-manifest.d65206de5c6c9592670712663909ef43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1a0c1dad50aae4041f34d773dddb4a",
+    "url": "/mbera124/github.ionp/index.html"
+  },
+  {
+    "revision": "a0ff72cb6b5d3830d2ca",
+    "url": "/mbera124/github.ionp/static/css/main.96e9d931.chunk.css"
+  },
+  {
+    "revision": "007ba04aae1c7385c20c",
+    "url": "/mbera124/github.ionp/static/js/2.50e77805.chunk.js"
+  },
+  {
+    "revision": "a0ff72cb6b5d3830d2ca",
+    "url": "/mbera124/github.ionp/static/js/main.6ac8e895.chunk.js"
+  },
+  {
+    "revision": "cb6329dc4c1ff339ecff",
+    "url": "/mbera124/github.ionp/static/js/runtime-main.ccb6c60f.js"
+  }
+]);
